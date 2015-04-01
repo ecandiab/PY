4b7cc347b8fc429fb4cbcf972bb5b8af524fd3c6
EC <ejcb87@gmail.com>
2015-04-01 11:24:13 -0300

    Ejercicio N6
    final

--- ejercicio-6.py
+++ ejercicio-6.py
@@ -8,36 +8,49 @@
 # * es     *
 # * una    *
 # * prueba *
-# **********
+# ********** 
 
 frase = "hola como estas"
 frase2 = "muy bien gracias y tu"
 frase3 = "yo esto muuuuuuy bieeeen aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa aa"
 frase4 = "excelente estas genial"
+frase5 = ""
+frase6 = "hola"
 
 def caja(lista):
 
 	j = 0
-	
-	for indice in range(len(lista)):
-		largoPalabra =  len(lista[indice])
-		if largoPalabra > len(lista[j]):
-			maxPalabra = largoPalabra
-			j = j+1
+	L = [] 
+
+	if len(lista) > 0: # si lista es vacia... retorna mensaje
+		for indice in range(len(lista)): # recorre la lista
+			largoPalabra =  len(lista[indice]) # en una variable almacena el largo de cada indice
+			L.append(largoPalabra) # guarda el largo de los elementos en lista vacia L
+
+			maxPalabra = max(L) # determina el mayor para determinar techo, base y pared derecha de asteriscos *
+			asterisco = "*"*maxPalabra # crea el tamanio de techo/base de acuerdo a la mayor longitud de elemento en  lista
+
+		lista.insert(0, asterisco) # techo *
+		lista.append(asterisco) # base *
 		
-			asterisco = "*"*maxPalabra
 
-	lista.insert(0, asterisco)
-	lista.append(asterisco)
-	
-
-	for indice in range(len(lista)):
-		espacios = " "*(maxPalabra-len(lista[indice]))
-
-		print "*"+lista[indice]+espacios+"*"
+		for indice in range(len(lista)): # recorre la lista 
+			espacios = " "*(maxPalabra-len(lista[indice])) # pared derecha *
+			print "*"+lista[indice]+espacios+"*" # devuelve la lista en caja
+	else:
+		print "***Lista Vacia*** \n"
 
 
-
-print caja(frase3.split())
-
+# Test 
+caja(frase.split())
+print "\n"
+caja(frase2.split())
+print "\n"
+caja(frase3.split())
+print "\n"
+caja(frase4.split())
+print "\n"
+caja(frase5.split())
+print "\n"
+caja(frase6.split())
 
